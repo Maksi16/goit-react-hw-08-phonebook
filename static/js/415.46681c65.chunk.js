eeb565e900e89ab66842794746d03a3ec4e975ad
Maksi16 <97921034+Maksi16@users.noreply.github.com>
2022-12-04 00:39:30 +0000

    Deploying to gh-pages from @ Maksi16/goit-react-hw-08-phonebook@d280575d84e70063cbb96934dc7da2f8782086fd 🚀

---
+++ static/js/415.46681c65.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var r,o=n(168),c=n(6444),a=n(184),h=c.ZP.h1(r||(r=(0,o.Z)(["\n  text-align: center;\n  margin-top: 50px;\n"])));function u(){return(0,a.jsx)("div",{children:(0,a.jsx)(h,{children:" Welcome to the PhoneBook! "})})}}}]);
+//# sourceMappingURL=415.46681c65.chunk.js.map
